2991edcd6ac1c84e9596aab3cade1942d6e8efba
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-11-17 18:14:28 -0200

    Todo button handler

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -5,11 +5,29 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {description: '', list: []}
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
+    }
+    
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm></TodoForm>
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}/>
                 <TodoList></TodoList>
             </div>
         )
